91f7516f954f7a5722f91fc9d5547fb63675f739
iHaeresis <arthurnegoreis@gmail.com>
2021-12-07 22:58:52 -0300

    finalização do escolas

--- app/Models/Escola.php
+++ app/Models/Escola.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Escola extends Model
 {
     use HasFactory;
+
+    //Laravel provides by default a protection against mass assignment security issues.
+    //That's why you have to manually define which fields could be "mass assigned" :
+
+    protected $fillable = ['escola_nome', 'escola_cep', 'escola_rua', 'escola_numero', 'escola_telefone', 'escola_email', 'escola_bairro'];
+
 }
